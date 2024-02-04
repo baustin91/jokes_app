f6460b88329dac7eadcc677a54bc65c174c6db5a
baustin91 <baustin14@my.gcu.edu>
2024-02-03 19:06:13 -0700

    Build 1.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5a564d1a2bdf0e1cbc2f29d89324390cb7e6bea0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5a564d1a2bdf0e1cbc2f29d89324390cb7e6bea0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
