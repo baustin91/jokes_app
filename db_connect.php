5710501177be222c4b308303951a58762f0633cc
baustin91 <baustin14@my.gcu.edu>
2024-02-03 18:42:29 -0700

    Project Build 1.0

---
+++ db_connect.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+// modify these settings according to the account on your database server.
+$host = "DB_HOST";
+$port = "3306";
+$username = "DB_USER";
+$user_pass = "DB_PASS";
+$database_in_use = "DB_NAME";
+
+
+$mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
+if ($mysqli->connect_error) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+echo $mysqli->host_info . "<br>";
